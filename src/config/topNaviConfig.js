50043b04800134a964e317f423936be5f917fcb5
qiujqi <805290715@qq.com>
2023-09-21 11:00:28 +0800

    init

---
+++ src/config/topNaviConfig.js
@@ -0,0 +1,11 @@
+export default {
+  leftTitle: 'SBS',
+  primaryTitle: [
+    // {title: '概览', path: '/overview'},
+    {title: '镜像', path: '/image'},
+    {title: '容器', path: '/container'},
+    {title: '创建', path: '/build'},
+    //{title: '任务', path: '/task'},
+    //{title: '运维', path: '/maintenance'}
+  ],
+}
